8b00684d58f2326892f25c9140ea0de8c59e972e
carlosvchrdevelop <carlosvchrdevelop@gmail.com>
2023-02-14 10:47:22 +0100

    Fixed exports and added comments.

--- jslikeutils.go
+++ jslikeutils.go
@@ -1,6 +1,10 @@
 package jslikeutils
 
-func smap[T any](arr []T, f func(e T, i int) T) []T {
+// Map applies a function to each element of array and returns it.
+// Function f receives the parameter e, which is the element of
+// the array that is currently being processed, and parameter i,
+// which is the index. It should return the processed element.
+func Map[T any](arr []T, f func(e T, i int) T) []T {
 	nArr := make([]T, len(arr))
 	for i, e := range arr {
 		nArr[i] = f(e, i)
@@ -8,7 +12,12 @@ func smap[T any](arr []T, f func(e T, i int) T) []T {
 	return nArr
 }
 
-func filter[T any](arr []T, f func(e T, i int) bool) []T {
+// Filter returns only elements of array which satisfies a condition.
+// Function f receives the parameter e, which is the element of
+// the array that is currently being processed, and parameter i,
+// which is the index. It should return a boolean indicating whether
+// that element must be included or not.
+func Filter[T any](arr []T, f func(e T, i int) bool) []T {
 	nArr := make([]T, 0, len(arr))
 	for i, e := range arr {
 		if f(e, i) {
@@ -18,7 +27,12 @@ func filter[T any](arr []T, f func(e T, i int) bool) []T {
 	return nArr
 }
 
-func reduce[T any](arr []T, f func(e T, n T, i int) T) T {
+// Reduce returns the result of procesing all elements in given array.
+// Function f receives the parameter e, which is the element that contains
+// the result of previous iteration, parameter n, that is the element of
+// the array that is currently being processed, and parameter i,
+// which is the index. It should return the processed element.
+func Reduce[T any](arr []T, f func(e T, n T, i int) T) T {
 	var res T
 	for i, e := range arr {
 		res = f(res, e, i)
